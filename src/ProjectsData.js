73fc0a9195175c0b095f7346f4214d03f7a440e8
baptistek02 <baptisteklempka@gmail.com>
2025-06-20 20:30:43 +0200

    remove portfolio, add singlecell, edit img fittracker

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -22,16 +22,6 @@ const dataProjects = [
     },
     {
         img: {
-            src: ["/assets/welcome.webp"],
-            alt: "welcome pic"
-        },
-        title: "My Portfolio",
-        shortdesc: "Take a look :)",
-        desc: "This portfolio was created with React JS",
-        cat: "Web"
-    },
-    {
-        img: {
             src: ["/assets/accessory2.png", "/assets/accessory1.png"],
             alt: "accessoryshop pic"
         },
@@ -42,12 +32,22 @@ const dataProjects = [
     },
     {
         img: {
-            src: ["/assets/img_fit1.PNG", "/assets/img_fit2.PNG", "/assets/img_fit3.PNG", "/assets/img_fit4.PNG"],
+            src: ["/assets/fittracker1.webp", "/assets/fittracker2.webp", "/assets/fittracker3.webp", "/assets/fittracker4.webp"],
             alt: "fitness tracker pic"
         },
         title: "Fitness Tracker",
         shortdesc: "Fitness tracking app built with React Native (Expo + TypeScript), Next.js API, and Supabase. Track workouts, create custom sessions, view progress charts, and manage user profiles.",
         desc: "Developed a fitness tracking app using React Native, Next.js API routes, and Supabase. Features include workout tracking, custom sessions, progress visualization with React Chart.js, and user authentication. Gained experience in fullstack mobile development and API management.",
+        cat: "Web"
+    },
+    {
+        img: {
+            src: ["/assets/singlecell.webp"],
+            alt: "singlecell pic"
+        },
+        title: "Singlecell Explorer",
+        shortdesc: "Web app for exploring single-cell genetic data. Built with React TypeScript, Python (Anndata, LoomPy), Docker, and Plotly.js. Optimized for performance with caching and efficient API requests.",
+        desc: "Developed SingleCell Explorer for SkinMap, a web application for interactive visualization of single-cell genetic data. Processed large Loom and H5AD files using Anndata and LoomPy in Python, with server-side caching for faster file access. Built performant, dynamic visualizations (boxplots, hexbins…) using Plotly.js and D3.js, leveraging WebGL for smooth rendering. Optimized API communication with reduced payloads and integrated React Query for client-side caching and lazy loading. The app was containerized with Docker to ensure easy deployment and reproducibility.",
         cat: "Web"
     },
 ]
